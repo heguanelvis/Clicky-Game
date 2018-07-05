f41951267aa796911e6e90e94be681de3954e581
Guan <heguanelvis@gmail.com>
2018-07-05 16:47:02 -0500

    reconfig the css using css modules

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={classes.App}>
+        <header className={classes.Appheader}>
+          <img src={logo} className={classes.Applogo} alt="logo" />
+          <h1 className={classes.Apptitle}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={classes.Appintro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
