9f95754dfc2ca6acc122c929c664283d2973350d
heguanelvis <heguanelvis@gmail.com>
2019-07-08 12:48:25 -0400

    Updates

---
+++ precache-manifest.0fc7d259365ae706f3c3ef1ebe6f80e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3c30122f818b57f71b9009deca273e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a8f25f8effa25fb4bff9",
+    "url": "/clicky-game/static/css/main.3fbba8f5.chunk.css"
+  },
+  {
+    "revision": "e4955b584cf5cf7a3137",
+    "url": "/clicky-game/static/js/2.c91a0e12.chunk.js"
+  },
+  {
+    "revision": "a8f25f8effa25fb4bff9",
+    "url": "/clicky-game/static/js/main.e9957439.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
